7610ee9ae3ab40fd72f2eb327cf49c830a11faf5
Levente Bajczi <bajczilevi@gmail.com>
2020-03-08 21:52:57 +0100

    added baremetal example

--- task7-gpio-mirror-baremetal/rpi.c
+++ task7-gpio-mirror-baremetal/rpi.c
@@ -1,10 +1,47 @@
-#define CONFIG   
-#define CFG_BIT  
-#define SET      
-#define CLEAR    
-#define DATA_BIT 
+// #define RPI2B
+#define RPI3BPLUS
+
+#define IOBASE   0x3f000000
+#define GPIO     0x200000
+
+#ifdef RPI2B
+
+#define CONFIG   0x00010
+#define CFG_BIT  21
+#define SET      0x00020
+#define CLEAR    0x0002C
+#define DATA_BIT 15
+
+#elif defined RPI3BPLUS
+
+#define CONFIG   0x00008
+#define CFG_BIT  27
+#define SET      0x0001C
+#define CLEAR    0x00028
+#define DATA_BIT 29
+
+#endif
+
+volatile void sleep(int seconds)
+{
+    for(int i = 0; i<seconds*10000000; ++i) asm volatile("nop");
+}
 
 int main()
 {
+    volatile unsigned int *set = (unsigned int *)(IOBASE + GPIO + SET);
+    volatile unsigned int *clr = (unsigned int *)(IOBASE + GPIO + CLEAR);
+    volatile unsigned int *cfg = (unsigned int *)(IOBASE + GPIO + CONFIG);
+
+    *cfg |= (1 << CFG_BIT); // set as output (only affects that bit)
+    for(int i = 0; i < 10; ++i) // flash the led 10 times
+    {
+        *set |= (1 << DATA_BIT);    // LED ON
+        sleep(1);
+        *clr |= (1 << DATA_BIT);    // LED OFF
+        sleep(1);
+    }    
+
     return 0;
+
 }
---
+++ task7-gpio-mirror-baremetal/start.s
@@ -0,0 +1,50 @@
+.globl _start
+_start:
+// ---------------------------------------------------------------------- Switch to Supervisor mode
+  mrs r0, cpsr
+  bic r0, r0, #0x1F     // clear mode bits
+  orr r0, r0, #0x13     // set Supervisor mode
+  msr spsr_cxsf, r0
+  add r0, pc, #4        // hold (in ELR_hyp) the address to return to  (to make 'eret' working right)
+  msr ELR_hyp, r0       // save the address in ELR_hyp
+  eret                  // apply the mode change (Exception return)
+
+  // ---------------------------------------------------------------------- Fill VBAR
+  mrc p15, 0, r1, c12, c0, 0              // get VBAR
+  mov r0, #0x8000                         // the address of our 8 handlers
+
+  ldmia r0!, {r2,r3,r4,r5, r6,r7,r8,r9}   // load multiple registers from consecutive memory locations using an address from the register r0
+  stmia r1!, {r2,r3,r4,r5, r6,r7,r8,r9}   // fill VBAR
+  ldmia r0!, {r2,r3,r4,r5, r6,r7,r8,r9}   // continue filling VBAR...
+  stmia r1!, {r2,r3,r4,r5, r6,r7,r8,r9}
+
+  // ---------------------------------------------------------------------- Init cache
+  mov r12,#0
+  mcr p15, 0, r12, c7, c10, 1   // DCCMVAC - Clean data or unified cache line by virtual address to PoC.
+  dsb                           // Data sync barrier
+  mov r12, #0
+  mcr p15, 0, r12, c7, c5, 0    // ICIALLU - Invalidate all instruction caches to PoU. If branch predictors are architecturally visible, also flush them.
+  mov r12, #0
+  mcr p15, 0, r12, c7, c5, 6    // BPIALL  - Invalidate all entries from branch predictors.
+  dsb                           // Data sync barrier
+  isb                           // Instruction sync barrier
+
+  // ---------------------------------------------------------------------- Turn on Instruction cache
+  mrc p15,0,r2,c1,c0,0
+  orr r2, #0x1000    // Instruction cache
+  mcr p15,0,r2,c1,c0,0
+
+  mov sp, #0x30000000
+  ldr r0,=0xDEADBEEF
+  ldr r1,=0x400000bc
+  str r0,[r1]
+  bl main
+  ldr r0,=0xFFFFFFFF
+  ldr r1,=0x400000fc
+  str r0,[r1]
+  mov r3, #0           // Comparison value
+_check_loop:             // Loop to check jump address
+    ldr r0, [r1]         // Get its value
+    cmp r0, r3           // Is it null?
+    beq _check_loop      // If yes, loop around
+    bx  r0               // If not, jump to the address
